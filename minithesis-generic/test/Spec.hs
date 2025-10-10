d83670f713a2159ad462917a44d1ff3df654850b
Mark Wotton <mwotton@gmail.com>
2025-10-10 09:14:55 +0700

    formatting

--- minithesis-generic/test/Spec.hs
+++ minithesis-generic/test/Spec.hs
@@ -1,7 +1,1 @@
-module Main (main) where
-
-import qualified Minithesis.GenericSpec as GenericSpec
-import Test.Syd (sydTest)
-
-main :: IO ()
-main = sydTest GenericSpec.spec
+{-# OPTIONS_GHC -F -pgmF sydtest-discover #-}
--- minithesis-sydtest/test/Spec.hs
+++ minithesis-sydtest/test/Spec.hs
@@ -1,11 +1,1 @@
-module Main (main) where
-
-import qualified Examples.SydtestSpec as Examples
-import qualified Selectorsleuth.PropertySpec as Selectorsleuth
-import Test.Syd
-
-main :: IO ()
-main =
-  sydTest $ do
-    Examples.spec
-    Selectorsleuth.spec
+{-# OPTIONS_GHC -F -pgmF sydtest-discover #-}
